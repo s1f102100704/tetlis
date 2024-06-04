d1ae7b375f8c244f1e1d340f32008a4b7eb3314d
s1f102100704 <s1f102100704@iniad.org>
2024-06-05 02:56:53 +0900

    とりあえず外枠

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -5,13 +5,59 @@ interface Props {
 }
 const GameBoard: React.FC<Props> = (props) => {
   const { gameMap } = props;
+  interface blockFrame {
+    side: number[][];
+    topBottom: number[];
+  }
+  const blockFrame: blockFrame = {
+    side: [],
+    topBottom: [],
+  };
+  const makeFrame = () => {
+    for (let i = 0; i < 22; i++) {
+      blockFrame.side.push([0]);
+    }
+    for (let i = 0; i < 10; i++) {
+      blockFrame.topBottom.push(0);
+    }
+  };
+  makeFrame();
   return (
-    <div className={styles.gameBoard}>
-      {gameMap.map((row, y) =>
-        row.map((color, x) => (
-          <div className={styles.cellstyle} key={`${x}-${y}`} id={`${x}-${y}`} />
-        )),
-      )}
+    <div className={styles.fullBoard}>
+      <div className={styles.sideFrame}>
+        {blockFrame.side.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.blockstyle} key={`${x}-${y}`} id={`${x}-${y}`} />
+          )),
+        )}
+      </div>
+      <div className={styles.boardPlus}>
+        <div className={styles.topBottomFrame}>
+          {blockFrame.topBottom.map(() => (
+            <div className={styles.blockstyle} key="" />
+          ))}
+        </div>
+        <div className={styles.gameBoard}>
+          {gameMap.map((row, y) =>
+            row.map((color, x) => (
+              <div className={styles.cellstyle} key={`${x}-${y}`} id={`${x}-${y}`} />
+            )),
+          )}
+        </div>
+        <div className={styles.topBottomFrame}>
+          {blockFrame.topBottom.map(() => (
+            <div className={styles.blockstyle} key="" />
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.sideFrame}>
+        {blockFrame.side.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.blockstyle} key={`${x}-${y}`} id={`${x}-${y}`} />
+          )),
+        )}
+      </div>
     </div>
   );
 };
