fc6959af81b2db0aaca0e1dc8baae887a6abc0d1
s1f102100704 <s1f102100704@iniad.org>
2024-06-11 10:29:19 +0900

    a

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -73,69 +73,75 @@ export const useGame = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
-
+  const cMino = Array.isArray(cloneNextMinos) ? cloneNextMinos.shift() : undefined;
   const outPutMino = useCallback(() => {
-    const cMino = Array.isArray(cloneNextMinos) ? cloneNextMinos.shift() : undefined;
-    const minoWidth = cMino?.direction?.[0]?.[0]?.length;
-    const minoHeight = cMino?.direction[0].length;
-    console.log(minoHeight);
-    if (cMino !== undefined && minoHeight !== undefined && minoWidth !== undefined) {
-      for (let i = 0; i < minoHeight; i++) {
-        for (let k = 3; k < minoWidth + 3; k++) {
-          cloneMinoMap[i][k] = cMino.direction[0][i][k - 3];
-        }
-      }
-      setMinoMap(cloneMinoMap);
-      setNextMinos(cloneNextMinos);
+    // const minoWidth = cMino?.direction?.[0]?.[0]?.length;
+    // const minoHeight = cMino?.direction[0].length;
 
+    // if (cMino !== undefined && minoHeight !== undefined && minoWidth !== undefined) {
+    //   for (let i = 0; i < minoHeight; i++) {
+    //     for (let k = 3; k < minoWidth + 3; k++) {
+    //       cloneMinoMap[i][k] = cMino.direction[0][i][k - 3];
+    //     }
+    //   }
+    // setMinoMap(cloneMinoMap);
+    // }
+    if (cMino !== undefined) {
+      setNextMinos(cloneNextMinos);
       setCurrentMino(cMino);
     }
-  }, [cloneMinoMap, cloneNextMinos, setNextMinos]);
+  }, [cloneNextMinos, setNextMinos, cMino]);
   useEffect(() => {
     const clearMino = (count: number) => {
-      for (let j = 0; j <= 9; j++) {
-        cloneMinoMap[count][j] = 0;
-        cloneMinoMap[count][j] = 0;
+      if (count !== 0) {
+        for (let j = 0; j <= 9; j++) {
+          cloneMinoMap[count - 1][j] = 0;
+          cloneMinoMap[count - 1][j] = 0;
+        }
       }
-      for (let k = 0; k <= 2; k++) {
-        cloneMinoMap[1 + count + 1][k + besideCount] = 0;
-      }
+
+      // for (let k = 0; k <= 2; k++) {
+      //   cloneMinoMap[1 + count][k + besideCount] = 0;
+      // }
     };
     const dropMino = () => {
-      const minoWidth = currentMino.direction[0][0].length;
-      const minoHeight = currentMino.direction[0].length;
-      for (let i = 0; i < minoHeight; i++) {
-        for (let k = 3; k < minoWidth + 3; k++) {
-          if (count <= 19 - minoHeight) {
-            if (
-              count + minoHeight !== 20 &&
-              cloneMinoMap[count + minoHeight][k + besideCount] !== 0
-            ) {
-              console.log('a');
-              setCount(0);
-              outPutMino();
-              setMinoMap(cloneMinoMap);
-              return 0;
-            } else {
-              cloneMinoMap[i + count + 1][k + besideCount] = currentMino.direction[0][i][k - 3];
-            }
-            if (count === 0) {
-              cloneMinoMap[0][k] = 0;
-              cloneMinoMap[0][k] = 0;
-            } else {
+      const minoWidth = cMino?.direction?.[0]?.[0]?.length;
+      const minoHeight = cMino?.direction[0].length;
+
+      console.log('minoheight', minoHeight);
+      if (cMino !== undefined && minoHeight !== undefined && minoWidth !== undefined) {
+        for (let i = 0; i < minoHeight; i++) {
+          for (let k = 3; k < minoWidth + 4; k++) {
+            if (count <= 20 - minoHeight) {
+              if (
+                count + minoHeight <= 19 &&
+                cloneMinoMap[count + minoHeight][k + besideCount] !== 0 &&
+                i === minoHeight - 1 &&
+                k === minoWidth + 3
+              ) {
+                setCount(0);
+                outPutMino();
+                setMinoMap(cloneMinoMap);
+                return 0;
+              } else {
+                cloneMinoMap[i + count][k + besideCount] = cMino.direction[0][i][k - 3];
+              }
+              //minoが下に下がったら、そのまえにあった minoを削除する
+
               clearMino(count);
             }
           }
         }
-      }
-      if (count === 20 - minoHeight) {
-        setCount(0);
-        outPutMino();
+        //minoが最下層についたら
+        if (count === 20 - minoHeight) {
+          setCount(0);
+          outPutMino();
+          setMinoMap(cloneMinoMap);
+          return 0;
+        }
+        setCount((prevCount) => prevCount + 1);
         setMinoMap(cloneMinoMap);
-        return 0;
       }
-      setCount((prevCount) => prevCount + 1);
-      setMinoMap(cloneMinoMap);
     };
     if (isPlay) {
       const interval = setInterval(dropMino, 200);
@@ -153,6 +159,7 @@ export const useGame = () => {
     currentMino.direction,
     outPutMino,
     besideCount,
+    cMino,
   ]);
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
